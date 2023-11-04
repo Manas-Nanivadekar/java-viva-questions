2528fb5c78e2bfac761740ec2690c51762e9fba6
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2023-11-04 16:53:14 +0530

    13th program

---
+++ Transpose.java
@@ -0,0 +1,53 @@
+// Write a java program to transpose of a matrix.
+
+import java.util.Scanner;
+
+public class Transpose {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter the elements of the matrix:");
+
+        // sc the matrix elements
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Transpose the matrix
+        int[][] transpose = new int[cols][rows];
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                transpose[i][j] = matrix[j][i];
+            }
+        }
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        System.out.println("Transposed Matrix:");
+        printMatrix(transpose);
+        sc.close();
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
