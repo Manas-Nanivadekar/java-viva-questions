5710152890b4c4baad98bced443eceaa7505412a
Jatin Khanijoan <146446897+Jatin-Khanijoan@users.noreply.github.com>
2023-11-05 21:24:50 +0530

    added vehicle and car classes to show single inheritance

--- SingleInheritance.java
+++ SingleInheritance.java
@@ -2,6 +2,43 @@
 
 import java.util.Scanner;
 
+class Vehicle {
+    String name;
+    String type;
+    String color;
+    int year;
+    double price;
+
+    public Vehicle(String name, String type, String color, int year, double price){
+        this.name = name;
+        this.type = type;
+        this.color = color;
+        this.year = year;
+        this.price = price;
+    }
+    
+    public void printVehicle() {
+    System.out.println("Vehicle name is "+name);
+    System.out.println("Vehicle type is "+type);
+    System.out.println("Vehicle color is "+color);
+    System.out.println("Vehicle year is "+year);
+    System.out.println("Vehicle price is "+price);
+}
+}
+
+class Car extends Vehicle {
+    String brand ;
+    public Car (String name, String type, String color, int year, double price, String brand){
+        super(name, type, color, year, price);
+        this.brand = brand;
+    }
+        
+        public void printCar() {
+            super.printVehicle();
+            System.out.println("Brand is "+brand);
+        }
+    }
+
 public class SingleInheritance {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,8 +52,11 @@ public class SingleInheritance {
         int year = sc.nextInt();
         System.out.println("Enter the price of the vehicle: ");
         double price = sc.nextDouble();
-        Vehicle v = new Vehicle(name, type, color, year, price);
-        v.printVehicle();
+        System.out.println("Enter brand of the Vehicle: ");
+        String brand = sc.nextLine();
+
+        Car c1 = new Car(name, type, color, year, price, brand);
+        v.printCar();
         sc.close();
     }
 }
